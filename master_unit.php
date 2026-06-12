f451045cbc271cbb19a8455ad9a45d4ffff700b2
Restu <palingganteng97@gmail.com>
2026-06-12 15:02:08 +0700

    Update master_unit.php

--- master_unit.php
+++ master_unit.php
@@ -109,7 +109,9 @@ if (isset($_POST['simpan_unit_baru'])) {
                     <a href="master_user.php" class="menu-item">Master User</a>
                     <a href="master_unit.php" class="menu-item active">Master Unit</a>
                     <a href="master_jabatan.php" class="menu-item" style="text-decoration: none;" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Master Jabatan</a>
-<a href="master_pendidikan.php" class="menu-item" style="text-decoration: none;" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Master Pendidikan</a>
+                    <a href="master_pendidikan.php" class="menu-item" style="text-decoration: none;" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Master Pendidikan</a>
+                    <a href="master_lowongan.php" class="menu-item" style="text-decoration: none;" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Master Lowongan</a>
+
                     <a href="user.php" class="menu-item">Profil Pengguna</a>
                 </nav>
             </div>
