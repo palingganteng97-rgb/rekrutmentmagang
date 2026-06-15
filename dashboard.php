db2a7a2ca727933a9e9012f5d9d24d7ceb3e7e30
Restu <palingganteng97@gmail.com>
2026-06-15 13:09:28 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -105,7 +105,7 @@ if (isset($_SESSION['username'])) {
                     <a href="master_pendidikan.php" class="menu-item" style="text-decoration: none;" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Master Pendidikan</a>
                     <a href="master_lowongan.php" class="menu-item" style="text-decoration: none;" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Master Lowongan</a>
                     <a href="master_tahapan_seleksi.php" class="menu-item">Master Tahapan Seleksi</a>
-                    <a href="user.php" class="menu-item" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Profil Pengguna</a>
+                    <a href="lowongan_tahapan.php" class="menu-item">Lowongan Tahapan</a>                    <a href="user.php" class="menu-item" onmouseover="this.style.background='#f8fafc'; this.style.color='#1e293b';" onmouseout="this.style.background='transparent'; this.style.color='#94a3b8';">Profil Pengguna</a>
                 </nav>
             </div>
             
