48ea91d4886998f975f3175fcaaff23a32e853d9
Restu <palingganteng97@gmail.com>
2026-06-15 13:09:37 +0700

    Update master_tahapan_seleksi.php

--- master_tahapan_seleksi.php
+++ master_tahapan_seleksi.php
@@ -133,6 +133,7 @@ $data_tahapan_edit = mysqli_fetch_assoc($query_edit);
                 <a href="master_pendidikan.php" class="menu-item">Master Pendidikan</a>
                 <a href="master_lowongan.php" class="menu-item">Master Lowongan</a>
                 <a href="master_tahapan_seleksi.php" class="menu-item active">Master Tahapan Seleksi</a>
+                <a href="lowongan_tahapan.php" class="menu-item">Lowongan Tahapan</a>
                 <a href="user.php" class="menu-item">Profil Pengguna</a>
             </div>
         </div>
